9e3910b2c66dfdfc2831117f430acf72f97d33a0
RamazanAliyev <aliyev.ramazan98@gmail.com>
2022-09-20 08:42:59 +0400

    first commit

---
+++ src/lib/header/Header.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from './svelte-logo.svg';
+</script>
+
+<header class="h-[200px] bg-black text-white flex justify-between">
+	<div class="corner">
+		<a href="/">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+	<nav data-sveltekit-prefetch>
+		<ul>
+			<li class="text-rose-400" class:active={$page.url.pathname === '/'}>
+				<a href="/">Home</a>
+			</li>
+			<li class:active={$page.url.pathname === '/about'}>
+				<a href="/about">About</a>
+			</li>
+			<li class:active={$page.url.pathname === '/todos'}>
+				<a href="/todos">Todos</a>
+			</li>
+		</ul>
+	</nav>
+</header>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main class="">
+	<slot />
+</main>
+
+<footer class="h-[100px] bg-black text-white flex justify-center items-center">
+	<p>Footer</p>
+</footer>
